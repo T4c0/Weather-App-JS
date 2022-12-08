b92c746dc7cb0d4a1e5f3c611cdf09b2d5829ede
Julian Vazquez <julianvazquez@MacBook-Pro.lan>
2022-12-08 09:52:05 -0600

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3efbad6a42msh7f510290a2fe00cp1ff3c2jsn56c24a3299fc",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3613f1afa156a1696cb2188da189c253";
